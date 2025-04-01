cc16a463e2ed3e342f15bab5a8ccc7ed661c454c
midhunmachari <midhunmachari@gmail.com>
2025-04-02 02:38:17 +0530

    edit

--- jobsub.sh
+++ jobsub.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 #SBATCH --gres=gpu:A100-SXM4:1
-#SBATCH --time=72:00:00
+#SBATCH --time=48:00:00
 
 # Display help message if "--help" is passed
 if [[ "$1" == "--help" ]]; then
